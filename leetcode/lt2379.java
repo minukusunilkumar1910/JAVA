a07568a84879dbe567f93f6b41aeee1949082840
Minuku.SunilKumar <154435816+minukusunilkumar1910@users.noreply.github.com>
2025-08-05 16:26:45 +0530

    Update lt2379.java
    ltcode 2379

--- leetcode/lt2379.java
+++ leetcode/lt2379.java
@@ -1,1 +1,33 @@
+public class pro {
+   public static void main(String[] args) {
+     
+   String blocks ="WBBWWBBWBW";
+   int k =7;
+    int i=0;
+        int j=i;
+        int minCount = k;
+        int count =0;
+        int n = blocks.length();
+        while(j<n)
+        {
+            if(blocks.charAt(j) == 'W')
+            {
+                count++;
+            }
+            if(j-i+1 == k && j<k)
+            {
+                minCount = Math.min(minCount,count);
+                
+                if(blocks.charAt(i) == 'W')
+                {
+                    count--;
+                }
+                i++;
+            }
+            j++;
+        }
+        System.out.println(minCount);
 
+   }
+
+}
